46cd4e7498a8004c8413a13071e1b77d93db486e
DrRomanSalvador <iglesiasroman@hotmail.es>
2026-09-14 23:47:59 +0200

    test: correct asymmetric spatial lag expectation

--- tests/core/test_spatial_matrix_contract.py
+++ tests/core/test_spatial_matrix_contract.py
@@ -20,4 +20,4 @@ def test_spatial_weights_can_be_asymmetric_when_each_row_has_valid_neighbours():
         [0.0, 4.0, 0.0],
     ]
     result = territorial_spatial_lag([1.0, 2.0, 5.0], weights)
-    assert np.allclose(result, [2.0, 4.25, 2.0])
+    assert np.allclose(result, [2.0, 4.0, 2.0])
